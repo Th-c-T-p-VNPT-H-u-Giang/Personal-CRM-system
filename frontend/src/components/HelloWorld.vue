d0d2d9fef82e30d62804b732c3e33bdb0d6eb835
lananh <anh626801@gmail.com>
2023-05-16 22:06:18 +0700

    test branch

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ const count = ref(0);
           <a class="nav-link" href="#">Danh sách khách hàng</a>
           <a class="nav-link" href="#">Danh sách sản phẩm</a>
           <a class="nav-link" href="#">Danh sách đơn hàng</a>
-          <a class="nav-link" href="#">Báo cáo 11</a>
+          <a class="nav-link" href="#">Báo cáo 111</a>
         </nav>
       </div>
       <div class="col-md-9">
