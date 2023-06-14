5b4efd1ac30090cf0f235417887650086499542d
lananh <anh626801@gmail.com>
2023-06-14 19:30:46 +0700

    employee_task

--- backend/app/controllers/task_employee.controller.js
+++ backend/app/controllers/task_employee.controller.js
@@ -1,137 +1,126 @@
-const { Permission, Role_Permission, Role, Task_Employee, Task, Employee } = require("../models/index.model.js");
+const { Employee_Task, Task, Employee } = require("../models/index.model.js");
 const createError = require("http-errors");
 const { v4: uuidv4 } = require("uuid");
 const { sequelize } = require("../config/index");
 
 exports.create = async (req, res, next) => {
-    if (Object.keys(req.body).length === 2) {
-        const { taskId, employeeId } = req.body;
-        const task_em = await Task_Employee.findAll();
-        for (let value of task_em) {
-            if (value.TaskId == taskId && value.EmployeeId == employeeId) {
-                return res.send({
-                    error: true,
-                    msg: `Đã tồn tại phân công .`,
-                });
-            }
-        }
-        try {
-            const document = await Task_Employee.create({
-                TaskId: taskId,
-                EmployeeId: employeeId,
-            });
-            return res.send({
-                error: false,
-                msg: `Bạn đã phân công thành công`,
-                document,
-            });
-        } catch (error) {
-            console.log(error.message);
-            return res.send({
-                error: true,
-                msg: error.message,
-            });
-        }
-    } else {
+  if (Object.keys(req.body).length === 2) {
+    console.log(req.body);
+    const { TaskId, EmployeeId } = req.body;
+    const task_em = await Employee_Task.findAll();
+    for (let value of task_em) {
+      if (value.TaskId == taskId && value.EmployeeId == employeeId) {
         return res.send({
-            error: true,
-            msg: `Vui lòng nhập đủ thông tin.`,
+          error: true,
+          msg: `Đã tồn tại phân công .`,
         });
+      }
     }
+    try {
+      const document = await Employee_Task.create({
+        TaskId: TaskId,
+        EmployeeId: EmployeeId,
+      });
+      return res.send({
+        error: false,
+        msg: `Bạn đã phân công thành công`,
+        document,
+      });
+    } catch (error) {
+      console.log(error.message);
+      return res.send({
+        error: true,
+        msg: error.message,
+      });
+    }
+  } else {
+    return res.send({
+      error: true,
+      msg: `Vui lòng nhập đủ thông tin.`,
+    });
+  }
 };
 
 exports.findAll = async (req, res, next) => {
-    try {
-        const documents = await Task_Employee.findAll({
-            include: [{
-                model: Task,
-                // attribute: ['status','reason']
-            },
-            {
-                model: Employee,
-            },
-            ]
-
-        });
-        return res.send(documents);
-    } catch (error) {
-        console.log(error);
-        return next(createError(400, "Error finding all task!"));
-    }
+  try {
+    const documents = await Employee_Task.findAll({});
+    return res.send(documents);
+  } catch (error) {
+    console.log(error);
+    return next(createError(400, "Error finding all task!"));
+  }
 };
 
 exports.findOne = async (req, res, next) => {
-    try {
-        const documents = await Task_Employee.findOne({
-            where: {
-                _id: req.params.id,
-            },
-        });
-        return res.send(documents);
-    } catch (error) {
-        return next(createError(400, "Error finding task !"));
-    }
+  try {
+    const documents = await Employee_Task.findOne({
+      where: {
+        _id: req.params.id,
+      },
+    });
+    return res.send(documents);
+  } catch (error) {
+    return next(createError(400, "Error finding task !"));
+  }
 };
 
 exports.deleteOne = async (req, res, next) => {
-    try {
-        const documents = await Task_Employee.destroy({
-            where: {
-                _id: req.params.id,
-            }   
-        });
-        return res.send(`Xóa phân công thành công`);
-    } catch (error) {
-        return next(createError(400, "Lỗi không xóa được phân công !"));
-    }
-}
-
+  try {
+    const documents = await Employee_Task.destroy({
+      where: {
+        _id: req.params.id,
+      },
+    });
+    return res.send(`Xóa phân công thành công`);
+  } catch (error) {
+    return next(createError(400, "Lỗi không xóa được phân công !"));
+  }
+};
 
 exports.update = async (req, res, next) => {
-    console.log('update', req.body);
-    // console.log(req.body.Status_Task.status);
-    // console.log(req.body.Status_Task.reason);
-    const { TaskId, EmployeeId } = req.body;
-    try {
-        let task_employee = [await Task_Employee.findOne({
-            where: {
-                _id: req.params.id,
-            },
-            include: [{
-                model: Task,
-                
-            },
-            {
-                model: Employee
-            }
-            ]
-        })];
+  console.log("update", req.body);
+  // console.log(req.body.Status_Task.status);
+  // console.log(req.body.Status_Task.reason);
+  const { TaskId, EmployeeId } = req.body;
+  try {
+    let Employee_Task = [
+      await Employee_Task.findOne({
+        where: {
+          _id: req.params.id,
+        },
+        include: [
+          {
+            model: Task,
+          },
+          {
+            model: Employee,
+          },
+        ],
+      }),
+    ];
 
-        task_employee = task_employee.filter(
-            (value, index) => {
-                return value.TaskId === TaskId && value.EmployeeId == EmployeeId
-            }
-        )
+    Employee_Task = Employee_Task.filter((value, index) => {
+      return value.TaskId === TaskId && value.EmployeeId == EmployeeId;
+    });
 
-        if (tasks.length == 0) {
-            const document = await Task_Employee.update({
-                EmployeeId : req.body.EmployeeId,
-            }, { where: { _id: req.params.id }, returning: true, });
-            return res.send({
-                error: false,
-                msg: 'Dữ liệu đã được thay đổi thành công.',
-            })
-        } else {
-            return res.send({
-                error: true,
-                msg: 'Dữ liệu chưa được thay đổi.'
-            })
-        }
-
-    } catch (error) {
-        return next(
-            createError(400, 'Error update')
-        )
+    if (tasks.length == 0) {
+      const document = await Employee_Task.update(
+        {
+          EmployeeId: req.body.EmployeeId,
+        },
+        { where: { _id: req.params.id }, returning: true }
+      );
+      return res.send({
+        error: false,
+        msg: "Dữ liệu đã được thay đổi thành công.",
+      });
+    } else {
+      return res.send({
+        error: true,
+        msg: "Dữ liệu chưa được thay đổi.",
+      });
     }
-
-}
+  } catch (error) {
+    return next(createError(400, "Error update"));
+  }
+};
